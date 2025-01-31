1587b8045e4b74d656440ee6dda36313fb34220d
Mahasri02 <153530070+Mahasri02@users.noreply.github.com>
2025-01-31 08:57:29 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import userRouter from "./routes/UserRoutes.js";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connection successful!");
+  } catch (err) {
+    console.error("MongoDB connection failed: ", err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+
+const app = express();
+app.use(express.json());
+
+
+app.use("/api/users", userRouter);
+app.use('/uploads', express.static('uploads'));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
